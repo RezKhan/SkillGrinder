68f2c1f5ed3a96362468ebaa28c179f6b56b3e0a
Rez <probablyrez@gmail.com>
2022-07-22 20:17:28 +1000

    Minor changes to damage formula calculation function

--- js/updates.js
+++ js/updates.js
@@ -1,10 +1,22 @@
-function enemyDamageTurn() {
-    return Math.round((Math.random() * (enemy.level+skg.currentEnemy.level))+((enemy.power * skg.currentEnemySkill.coefficient) * (rankToCoefficient(enemy.rank) + (rankToCoefficient(skg.currentEnemy.rank) * rankToCoefficient(skg.currentEnemySkill.rank))) * (enemy.level + skg.currentEnemy.level))); 
-    
-}
+function enemyDamageTurn() { 
+    let primaryMultiplier = enemy.power;
+    let secondaryMultiplier = (enemy.level * skg.currentEnemy.level) + skg.currentEnemySkill.level;
+    let coefficient = rankToCoefficient(enemy.rank) + ((1 + skg.currentEnemySkill.coefficient) * rankToCoefficient(skg.currentEnemy.rank) * rankToCoefficient(skg.currentEnemySkill.rank))
+    let wiggle = Math.random() * (enemy.level+skg.currentEnemy.level);
 
+    let result = Math.round(wiggle + (primaryMultiplier * secondaryMultiplier * coefficient))
+    return result;
+}
+// TODO: replace these two with one function that takes 3 objects as arguments
 function adventurerDamageTurn() {
-    return Math.round((Math.random() * (adventurer.level + skg.currentJob.level)) + (adventurer.power * skg.currentSkill.coefficient * (rankToCoefficient(adventurer.rank, 1+skg.currentJob.tier) + (rankToCoefficient(skg.currentJob.rank, 1+skg.currentJob.tier) * rankToCoefficient(skg.currentSkill.rank, 11+skg.currentJob.tier))) * ((adventurer.level * skg.currentJob.level) + skg.currentSkill.level)));
+    let primaryMultiplier = adventurer.power;
+    let secondaryMultiplier = (adventurer.level * skg.currentJob.level) + skg.currentSkill.level;
+    let coefficient = rankToCoefficient(adventurer.rank, 1+skg.currentJob.tier) + ((1 + skg.currentSkill.coefficient) * rankToCoefficient(skg.currentJob.rank, 1+skg.currentJob.tier) * rankToCoefficient(skg.currentSkill.rank, 1+skg.currentJob.tier));
+    var wiggle = Math.random() * (adventurer.level + skg.currentJob.level);
+    // console.log('Primary = ', primaryMultiplier, 'Secondary = ', secondaryMultiplier, 'Coeff = ', coefficient, 'wiggle = ', wiggle);
+
+    let result = Math.round(wiggle + (primaryMultiplier * secondaryMultiplier * coefficient));
+    return result;
 }
 
 function rankToCoefficient(rank, modifier) {
@@ -18,10 +30,6 @@ function rankToCoefficient(rank, modifier) {
             return (0.5*modifier);
     }
 }
-
-
-
-
 
 function joblessUnlocks() {
     if (adventurer.job[0].abilities[0].level >= 3) { // unlock kick
@@ -40,3 +48,21 @@ function mageUnlocks() {
 function checkUnlocks() {
     joblessUnlocks();
 }
+
+// Zpecs thing, use this when I need something more robust, probably when gear is in place
+// function zspecsDamage() {
+//     const modifiers = [
+//         { name: "Sword of Damage Tenfolding", transform: x => x * 10 },
+//         { name: "Helmet of Non-Seeing", transform: x => x - 2 }
+//       ];
+      
+//       function calculateModifiers(value) {
+//         let result = [value];
+//         for (const modifier of modifiers) {
+//           result = result.map(modifier.transform)
+//         }
+//         return result[0];
+//       }
+      
+//       const newDamage = calculateModifiers(oldDamage);
+// }
